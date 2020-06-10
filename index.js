5686056f0c838dc9a3a72932010ecc24943ab7ac
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-10 13:43:18 +0300

    setting simple CLI to manage DB

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const { CRUD } = require("./helpers");
 const db = require("./models");
 
 const params = process.argv;
-console.log(params);
+
+if (params.length <= 2) {
+  process.exit();
+}
+
+const args = params.slice(2);
+
+const command = args[0].split(":")[0].substring(2);
+const entity = args[0].split(":")[1];
+
+switch(command) {
+  case CRUD.CREATE:
+    console.log("CREATED");
+    break;
+  case CRUD.READ:
+    console.log("READ")
+    break;
+  default:
+    console.log("Operation not found!!!")
+}
